6c025db830832194c95d2177e89b0a5a68535029
dazzie <daragh_moran@yahoo.com>
2024-05-05 09:56:15 -0700

    Update questions.py

--- questions.py
+++ questions.py
@@ -36,6 +36,38 @@ openai = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 df = pd.read_csv('processed/embeddings.csv', index_col=0)
 df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
 
+def create_context(question, df, max_len=1800):
+  """
+    Create a context for a question by finding the most similar context from the dataframe
+    """
+  # Get the embeddings for the question
+  q_embeddings = openai.embeddings.create(
+      input=question, model='text-embedding-ada-002').data[0].embedding
+
+  # Get the distances from the embeddings
+  df['distances'] = distances_from_embeddings(q_embeddings,
+                                              df['embeddings'].values,
+                                              distance_metric='cosine')
+
+  returns = []
+  cur_len = 0
+
+  # Sort by distance and add the text to the context until the context is too long
+  for i, row in df.sort_values('distances', ascending=True).iterrows():
+    # Add the length of the text to the current length
+    cur_len += row['n_tokens'] + 4
+
+    # If the context is too long, break
+    if cur_len > max_len:
+      break
+
+    # Else add it to the text that is being returned
+    returns.append(row["text"])
+
+  # Return the context
+  return "\n\n###\n\n".join(returns)
+
+
 def answer_question(df,
                     model="gpt-3.5-turbo-1106",
                     question="What is the meaning of life?",
